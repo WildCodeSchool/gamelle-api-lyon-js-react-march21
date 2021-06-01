3b6e32cfd656a5d2f79ad4ecffb9b0124c09fdab
Cécile Dubois <duboiscecilepro@gmail.com>
2021-06-01 12:31:54 +0200

    Started to add connection to api

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// const axios = require('axios');
 const express = require('express');
 const { inTestEnv, PORT } = require('./env');
 
@@ -7,12 +8,52 @@ app.set('x-powered-by', false);
 app.use(express.json());
 
 require('./routes')(app);
+
+// const delayInMilliseconds = 60 * 60 * 1000;
+
 // server setup
 const server = app.listen(PORT, () => {
   if (!inTestEnv) {
     console.log(`Server running on port ${PORT}`);
   }
 });
+
+/* ********************** Connection & request to API periodically ********************** */
+// const gamelleBreedsRequest = () => {
+//   const firstRequest = connection
+//     .promise()
+//     .query(
+//       'SELECT ingredients_id AS id, ingredients_ingr AS ingr FROM ingredients'
+//     )
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+//   firstRequest
+//     .then((reqresult) => {
+//       const listOfIngredients = Object.values(
+//         JSON.parse(JSON.stringify(reqresult[0]))
+//       );
+//       listOfIngredients.forEach((ingredient) =>
+//         axios
+//           .get(`https://gamelle-manager-staging.herokuapp.com/api`)
+//           .then((response) => response.data)
+//           .then((data) => {
+//             connection
+//               .promise()
+//               .query(
+//                 `UPDATE Food SET ingredients_kcal100 = ? WHERE ingredients_id = ?;`,
+//                 [data.parsed[0].food.nutrients.ENERC_KCAL, ingredient.id]
+//               );
+//           })
+//       );
+//     })
+//     .catch((err) => console.log(err));
+//   console.log(`Request to API done at ${new Date().toLocaleString()}`);
+// };
+
+// edamamRequest();
+// setInterval(edamamRequest, delayInMilliseconds);
 
 // process setup
 process.on('unhandledRejection', (error) => {
