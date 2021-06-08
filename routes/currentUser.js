d64f1463a0f7c3245a7e136670b62064d011e730
Brandon Monge <oorekan@users.noreply.github.com>
2021-06-08 13:44:42 +0200

    Remove uppercase in currentUser.js

--- routes/currentUser.js
+++ routes/currentUser.js
@@ -2,14 +2,13 @@ const currentUserRouter = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const requireCurrentUser = require('../middlewares/requireCurrentUser');
 
-
 // --------- Caption of logged user info --------- //
 currentUserRouter.get(
   '/',
   requireCurrentUser,
   asyncHandler(async (req, res) => {
-    const { firstName, lastName, id } = req.currentUser;
-    res.json({ firstName, lastName, id });
+    const { firstname, lastname, email, id } = req.currentUser;
+    res.json({ firstname, lastname, email, id });
   })
 );
 
