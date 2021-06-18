3702ba62dcbc084354b83701e0cc24c1ad040ad4
Cécile Dubois <duboiscecilepro@gmail.com>
2021-06-18 15:05:41 +0200

    Corrected search request

--- models/search.js
+++ models/search.js
@@ -45,36 +45,22 @@ const findAnimalCategories = () => {
 };
 
 const findProducts = async ({
-  filters: { brand, foodTypeName, animalCategoryName, searchedWords },
+  filters: { brand, foodTypeId, animalCategoryId, searchedWords },
 }) => {
-  const animalCategoryResult = await db.animalCategory.findFirst({
-    where: { name: animalCategoryName },
-  });
-  const animalcategorySQL = animalCategoryResult
-    ? `AND animalCategoryId=${animalCategoryResult.id}`
-    : '';
-
-  const foodTypeResult = await db.foodType.findFirst({
-    where: { name: foodTypeName },
-  });
-  const foodTypeSQL = foodTypeResult
-    ? `AND foodTypeId=${foodTypeResult.id}`
-    : '';
-
   const words = searchedWords
     ? searchedWords.split(' ').filter((word) => word !== '')
     : [];
 
-  let searchText = '';
-
-  if (words !== [])
-    words.filter(Boolean).forEach((word) => {
-      searchText += `AND name LIKE '%${word.trim()}%'`;
-    });
-
-  return db.$queryRaw(
-    `SELECT * FROM Food WHERE brand="${brand}" ${animalcategorySQL} ${foodTypeSQL} ${searchText};`
-  ); // Attention ! Ne pas remplacer les guillemets doubles par des simples car la requête échoue avec les noms comportant un guillemet simple comme 4PAT'
+  return db.food.findMany({
+    where: {
+      brand,
+      animalCategoryId: animalCategoryId
+        ? parseInt(animalCategoryId, 10)
+        : undefined,
+      foodTypeId: foodTypeId ? parseInt(foodTypeId, 10) : undefined,
+      AND: words.map((word) => ({ name: { contains: word } })),
+    },
+  });
 };
 
 module.exports = { findProducts, findBrands, findTypes, findAnimalCategories };
