ca3c100da545ac99ab75de83ee703b086325267f
Cécile Dubois <duboiscecilepro@gmail.com>
2021-06-18 10:42:47 +0200

    History ok

--- models/history.js
+++ models/history.js
@@ -1,5 +1,28 @@
 const db = require('../db');
 
+const findHistories = async (id) => {
+  const userId = parseInt(id, 10);
+  return db.history.findMany({
+    orderBy: {
+      consultedAt: 'desc',
+    },
+    where: {
+      userId,
+    },
+    include: {
+      Foods: true,
+    },
+  });
+};
+
+const findHistoryDetails = async (id) => {
+  return db.food.findMany({
+    where: {
+      id,
+    },
+  });
+};
+
 const createHistory = async ({ filters: { userId, foodId } }) => {
   const fav = await db.favorite.findFirst({
     where: {
@@ -16,4 +39,8 @@ const createHistory = async ({ filters: { userId, foodId } }) => {
   });
 };
 
-module.exports = { createHistory };
+module.exports = {
+  createHistory,
+  findHistories,
+  findHistoryDetails,
+};
--- routes/histories.js
+++ routes/histories.js
@@ -1,5 +1,20 @@
 const historiesRouter = require('express').Router();
 const History = require('../models/history');
+const requireCurrentUser = require('../middlewares/requireCurrentUser');
+
+historiesRouter.get('/', requireCurrentUser, async (req, res) => {
+  const { id } = req.currentUser;
+
+  try {
+    const historyData = await History.findHistories(id);
+    return res.json(historyData);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send("Il y a eu une erreur lors de la récupération de l'historique");
+  }
+});
 
 historiesRouter.post('/', (req, res) => {
   const { userId, foodId } = req.body;
