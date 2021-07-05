a5c24646690669efd5002bd190dea5d98cd87c23
Brandon Monge <oorekan@users.noreply.github.com>
2021-07-05 14:27:55 +0200

    Remove if userId for now

--- middlewares/requireCurrentUser.js
+++ middlewares/requireCurrentUser.js
@@ -2,14 +2,12 @@ const User = require('../models/user');
 
 module.exports = async (req, res, next) => {
   const { userId } = req.session;
-  if (req.session.passport.user || userId) {
-    try {
-      req.currentUser = await User.findOne(
-        req.session.passport ? req.session.passport.user : userId
-      );
-    } catch (err) {
-      return res.sendStatus(401);
-    }
+  try {
+    req.currentUser = await User.findOne(
+      req.session.passport ? req.session.passport.user : userId
+    );
+  } catch (err) {
+    return res.sendStatus(401);
   }
   return next();
 };
