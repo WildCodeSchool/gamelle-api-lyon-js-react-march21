f00323b6abde50155ac56e3dd85ca4fec669abd3
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-07-12 11:40:21 +0200

    fix migrations

--- prisma/migrations/20210707144224_removed_ipv6/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ipv6Address` on the `statistics` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `statistics` DROP COLUMN `ipv6Address`;
